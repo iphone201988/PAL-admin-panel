7cd77baa4faab1f697ac0727052a3ba891349b5d
Developer <michaelmac@MichaelMac2024s-Mac-mini.local>
2025-07-25 16:11:54 +0530

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/admin/', // 👈 REQUIRED for subfolder deployment
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   },
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
 })
